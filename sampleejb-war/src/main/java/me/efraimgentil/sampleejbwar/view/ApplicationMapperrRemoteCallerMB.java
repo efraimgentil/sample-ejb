8d0737a0c1ab02dbf8532672cf6ff9e278229c0c
root <efraimgentil@gmail.com>
2016-09-18 14:15:00 -0300

    Added cors filter

--- sampleejb-war/src/main/java/me/efraimgentil/sampleejbwar/view/ApplicationMapperrRemoteCallerMB.java
+++ sampleejb-war/src/main/java/me/efraimgentil/sampleejbwar/view/ApplicationMapperrRemoteCallerMB.java
@@ -1,10 +1,10 @@
 package me.efraimgentil.sampleejbwar.view;
 
-import java.util.AbstractMap;
+import java.util.Hashtable;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
@@ -25,6 +25,48 @@ public class ApplicationMapperrRemoteCallerMB {
 	
 	private String key;
 	private String value;
+	
+	
+	public static void main(String[] args) {
+//		String num = "mmmmm";
+//		
+//		Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
+//	    ht.put('i',1);
+//	    ht.put('x',10);
+//	    ht.put('c',100);
+//	    ht.put('m',1000);
+//	    ht.put('v',5);
+//	    ht.put('l',50);
+//	    ht.put('d',500);
+//		
+//	    int intNum=0;
+//	    int prev = 0;
+//	    for(int i = num.length()-1; i>=0 ; i--)
+//	    {
+//	            int temp = ht.get(num.charAt(i));
+//	            if(temp < prev)
+//	                intNum-=temp;
+//	            else
+//	                intNum+=temp;
+//	            prev = temp;
+//	    }
+//	    System.out.println( intNum );
+//		int a = 1;
+//		int b = 2;
+//		
+//		int c = a & b;
+//		System.out.println( 1 & 2 );
+//		System.out.println( 1 & 3 );
+//		System.out.println( 1 & 4 );
+
+		String string = "saveCountTes";
+		Pattern compile = Pattern.compile( "(?<=[a-z])([A-Z])(?=[a-z])");
+		Matcher matcher = compile.matcher( string );
+		System.out.println( matcher.groupCount() );
+		while( matcher.find() ){
+			System.out.println( matcher.group() );
+		}
+	}
 	
 	public User getUser(){
 		return service.getValue("someKey", User.class);
