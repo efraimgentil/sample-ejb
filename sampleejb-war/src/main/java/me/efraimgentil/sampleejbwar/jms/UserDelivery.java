865bf8f404a90998cd6e47c4d9376fbd28a1049d
EFRAIM GENTIL DE GOES FILHO <008054L8@sefaz-ce.gov.br>
2016-09-21 08:08:43 -0300

    Message creator in the war project, to easyly test a load of data to a message bean

---
+++ sampleejb-war/src/main/java/me/efraimgentil/sampleejbwar/jms/UserDelivery.java
@@ -0,0 +1,31 @@
+package me.efraimgentil.sampleejbwar.jms;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.ObjectMessage;
+
+import me.efraimgentil.sampleejb.core.model.User;
+
+@Stateless
+public class UserDelivery {
+	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext jmsContext;
+	
+	@Resource(name="java:/jms/queue/ExpiryQueue")
+	private Destination destination;
+	
+	public void sentUserToQueue(User user ) throws JMSException{
+		ObjectMessage om = jmsContext.createObjectMessage();
+		om.setObject( user );
+		jmsContext.createProducer().send(destination, om );
+	}
+	
+	
+}
